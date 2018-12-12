f067d5e282a595deee0e93fc4b1bba11e55ba3d2
JSG01 <32953933+JSG01@users.noreply.github.com>
2018-12-12 16:27:47 +0100

    Update dynmotd.sh

--- src/dynmotd.sh
+++ src/dynmotd.sh
@@ -8,6 +8,12 @@
 #year            :2017
 #notes           :-none-
 
+#show weather
+curl wttr.in/hamburg?0
+
+#leerzeile
+echo " "
+
 #SetEscapeColorCodes
 CYN='\033[0;36m'
 RED='\033[0;31m'
@@ -19,12 +25,16 @@ NC='\033[0m' #NoColor
 #Ausgabe mit Farbe:
 printf "${CYN}$(sw_vers)\n\n"
 
-pydf --block-size=256000000000 /Volumes/Macintosh\ HD
+#pydf --block-size=256000000000 /Volumes/Macintosh\ HD
 printf "${YLW}$(diskutil info / | grep "Total Size:" | cut -b "1-38")\n"
 printf "${YLW}$(diskutil info / | grep "Volume Free Space:" | cut -b "1-38")\n"
 
 printf "${GRY}\n"
 top -l 1 -s 0 | grep "CPU usage"
+
+#leerzeile
+echo " "
+
 printf "${PUR}$(top -l 1 -s 0 | grep PhysMem)\n\n"
 
 printf "${YLW}$(istats cpu)\n"
@@ -33,6 +43,10 @@ printf "${YLW}$(istats fan)\n\n"
 printf "${RED}$(date)\n"
 printf "${GRY}"
 echo "Your external IP: "; curl ipinfo.io/ip
+
+echo " "
+printf "${CYN}$(echo "***********************************************")\n"
+
 
 #EndEscapeColorCode
 printf "${NC}\n\n"
